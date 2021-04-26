f39b365d45290f4c440fdd166ea9396c052227ba
madavila <davila19manuel117@gmail.com>
2021-04-26 11:56:29 -0600

    Sidenav Working and datepicker centered

--- transporte/ClientApp/src/app/toolbar/toolbar.component.ts
+++ transporte/ClientApp/src/app/toolbar/toolbar.component.ts
@@ -9,20 +9,9 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent{
 
   events: string[] = [];
   opened: boolean;
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) { }
-  panelOpenState = false;
-  ngOnInit(): void {
-  }
 
 }
